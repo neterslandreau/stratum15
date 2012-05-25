0a2c5bdd39ec09ddbf298cfaa4ecf599ea85dd03
Niles Rowland <neterslandreau@gmail.com>
2012-05-24 21:03:57 -0400

    get debug kit operating.

--- app_controller.php
+++ app_controller.php
@@ -8,12 +8,12 @@
  * PHP versions 4 and 5
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
  * @package       cake
  * @subpackage    cake.app
@@ -31,4 +31,87 @@
  * @subpackage    cake.app
  */
 class AppController extends Controller {
+/**
+ * This is the cookie name for the remember me feature. It is also used in app_users_controller::_setCookie()
+ *
+ * @var string
+ */
+	protected $_cookieName = 'stratumRememberMe';
+    public $components = array(
+		'Session',
+//		'Auth',
+		'Cookie',
+		'Email',
+		'RequestHandler',
+//		'Search.Prg',
+		'DebugKit.Toolbar' => array('panels' => array('Interactive.interactive')),
+	);
+	public $helpers = array(
+		'Session',
+		'Html',
+		'Javascript',
+		'Form',
+		'Xml',
+		'Text'
+	);
+	public $publicControllers = array('pages');
+/**
+ *
+ *
+	public function beforeFilter() {
+		// i like to be able to view emails in development from debug kit
+		if (Configure::read('debug')) {
+			$this->Email->delivery = 'debug';
+		}
+		$prefixes = Configure::read('Routing.prefixes');
+		$admin = in_array('admin', $prefixes);
+		$this->Auth->loginAction = array('plugin' => null, 'controller' => 'app_users', 'action' => 'login', 'admin' => false);
+		$this->Auth->logoutRedirect = '/';
+		$this->Auth->loginError = __('Invalid username / password combination.  Please try again', true);
+		$this->Auth->autoRedirect = false;
+		$this->Auth->authorize = 'controller';
+		$this->Auth->fields = array('username' => 'email', 'password' => 'passwd');
+		$this->Auth->userScope = array('User.email_authenticated' => 1);
+
+		$this->Cookie->name = $this->_cookieName;
+		$this->Cookie->time = '1 Month';
+		$cookie = $this->Cookie->read('User');
+
+		if (!empty($cookie) && !$this->Auth->user()) {
+			$data['User']['email'] = '';
+			$data['User']['passwd'] = '';
+			if (is_array($cookie)) {
+				$data['User']['email'] = $cookie['email'];
+				$data['User']['passwd'] = $cookie['passwd'];
+			}
+			if (!$this->Auth->login($data)) {
+				$this->Cookie->destroy();
+				$this->Auth->logout();
+			}
+		}
+		if ($this->Auth->user()) {
+			$this->set('userData', $this->Auth->user());
+		}
+
+		if (in_array(strtolower($this->params['controller']), $this->publicControllers)) {
+            $this->Auth->allow('*');
+        }
+	}
+/**
+ *
+ * @return boolean
+ *
+	public function isAuthorized() {
+		if (isset($this->params['prefix'])) {
+			if ($this->params['prefix'] == 'admin' && $this->Auth->user('is_admin')) {
+				return true;
+			}
+		} else {
+			if ($this->Auth->user()) {
+				return true;
+			}
+		}
+		return false;
+	}
+/* */
 }
